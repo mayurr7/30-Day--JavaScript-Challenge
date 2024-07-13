4e5bbdafa1957c4f6c6f7bcf1ade7b7df3e77bc6
Mayur Tekale <mayurtekale7@gmail.com>
2024-07-13 18:36:46 +0530

    I understand about variables and typeOf a variable

---
+++ 01-variableAndDatatype.js
@@ -0,0 +1,51 @@
+//This is day one  of my journey 
+//Variable and DataType
+
+//Activity 1 : Declaration
+
+var number = 10;
+console.log(number);
+console.log(typeof number);
+
+let name = 'mayur';
+console.log(name);
+console.log(typeof name);
+
+const isLoggedIn = true;
+console.log(isLoggedIn);
+console.log(typeof isLoggedIn);
+
+const array = [1, 2 , 3];
+console.log(array);
+console.log(typeof array);
+
+//task 2
+console.log( );
+let userName = 'mayuuu';
+userName = 'mayur'
+console.log(userName);
+
+console.log('underStanding const');
+const pass = 1234;
+
+//pass = 7458;
+console.log(pass);
+
+
+//Feature reques
+
+let course = 'javaScript';
+var  fees = 999;
+const durationOfCourse = 30;
+
+console.log(course);
+console.log(typeof course);
+console.log(fees);
+console.log(typeof fees);
+console.log(durationOfCourse);
+console.log(typeof durationOfCourse);
+
+
+
+
+
