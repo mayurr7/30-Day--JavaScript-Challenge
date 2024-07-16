ae1b8bd14015b6e12c07e74a3c45007dc1764db9
Mayur Tekale <mayurtekale7@gmail.com>
2024-07-16 21:17:15 +0530

    initial commit

--- 04-loops.js
+++ 04-loops.js
@@ -1,7 +1,34 @@
-//day 4 Loops
+// //day 4 Loops
 
-//1 for loop
+// //1 for loop
 
-for(let i = 0; i <=10; i++){
-    
-}
+// for(let i = 0; i <=10; i++){
+//     console.log(i);
+// }
+
+
+// for(let i = 0 ; i < 11 ; i++){
+//     console.log(i  * 5);
+// }
+
+// console.log("while loop");
+
+// let i =1;
+// let sum =0;
+// while(i < 11){
+//     sum+=i;
+//     console.log(sum);
+//     i++;
+// }
+
+//do while
+//let i =0;
+// do{
+//     console.log(i);
+//     i++;
+
+// }while(i <6)
+
+
+
+
